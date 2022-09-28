5b1ea17bcdede496ca1e8a029808f3baca26cad7
Brandon <figueredo407@gmail.com>
2022-09-28 11:31:25 -0400

    first commit

---
+++ PluralsightTutorial/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Startup
+{
+
+    public class HelloWorld
+    {
+        public int Num { get; init; }
+        public string Name { get; set; } = "brandon";
+    }
+
+    public class Startup
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+
+            Dictionary<string, int> dic = new();
+            while(stack.Any())
+            {
+                int num = stack.Pop();
+                Console.WriteLine(num);
+                dic.Add("brandon" + num, num);
+            }
+
+            foreach(var item in dic)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
